2df2acbdcc929eaeb970a3d4be6672ee3fb1fae9
Wishmaster01 <Bm9VBcAT>
2017-06-01 19:32:47 +0200

    Categorisation rework

--- source/FoodCategoryUtils.cs
+++ source/FoodCategoryUtils.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace WM.SmarterFoodSelection
@@ -150,131 +151,159 @@ namespace WM.SmarterFoodSelection
 			return category;
 		}
 
+		// There might be a cleaner way to do what looks like a deep neural network.
 		private static FoodCategory _DetermineFoodCategory(ThingDef def)
 		{
+			var array = Enum.GetValues(typeof(FoodCategory));
+
+			var CategoryScores = new Dictionary<FoodCategory, float>(array.Length);
+
+			foreach (FoodCategory entry in array)
+			{
+				CategoryScores.Add(entry, 0f);
+			}
+
 			if (def == ThingDefOf.NutrientPasteDispenser)
 			{
 				return FoodCategory.MealAwful;
 			}
 
 			if (def.race != null)
-				return FoodCategory.Hunt;
+				CategoryScores[FoodCategory.Hunt] = float.MaxValue;
 
 			if (def.ingestible != null)
 			{
 				if (def.ingestible.nutrition <= 0f || def.IsDrug)
-					return FoodCategory.Ignore;
+					CategoryScores[FoodCategory.Ignore] = float.MaxValue;
 
 				FoodPreferability foodPref = def.ingestible.preferability;
 				FoodTypeFlags foodType = def.ingestible.foodType;
 
 				string defName = def.defName;
 
-				if (foodPref == FoodPreferability.MealFine)
+				switch (foodPref)
+				{
+					case FoodPreferability.MealFine:
+						CategoryScores[FoodCategory.MealFine] += 100f;
+						break;
+					case FoodPreferability.MealAwful:
+						CategoryScores[FoodCategory.MealAwful] += 100f;
+						break;
+					case FoodPreferability.MealSimple:
+						CategoryScores[FoodCategory.MealSimple] += 100f;
+						break;
+					case FoodPreferability.MealLavish:
+						CategoryScores[FoodCategory.MealLavish] += 100f;
+						break;
+				}
 
-					return FoodCategory.MealFine;
-
-				if (foodPref == FoodPreferability.MealAwful)
-
-					return FoodCategory.MealAwful;
-
-				if (foodPref == FoodPreferability.MealSimple)
-
-					return FoodCategory.MealSimple;
-
-				if (foodPref == FoodPreferability.MealLavish)
-
-					return FoodCategory.MealLavish;
+				if (!def.HasComp(typeof(CompRottable)) && (foodType & FoodTypeFlags.Meal) != 0)
+				{
+					CategoryScores[FoodCategory.MealSurvival] += 150f;
+				}
 
 				if ((foodType & FoodTypeFlags.Kibble) != 0)
-
-					return FoodCategory.Kibble;
+					CategoryScores[FoodCategory.Kibble] += 200f;
 
 				if ((foodType & FoodTypeFlags.AnimalProduct) != 0)
 				{
 					if (def.GetCompProperties<CompProperties_Hatcher>() != null)
 
-						return FoodCategory.FertEggs;
+						CategoryScores[FoodCategory.FertEggs] += 500f;
 
 					//return WMFoodPref.Null;
 				}
 
 				if (def.ingestible.joyKind == JoyKindDefOf.Gluttonous && def.ingestible.joy >= 0.05f)
 
-					return FoodCategory.Luxury;
+					CategoryScores[FoodCategory.Luxury] += 500f;
 
 				if ((foodType & FoodTypeFlags.Tree) != 0)
 
-					return FoodCategory.Tree;
+					CategoryScores[FoodCategory.Tree] += 500f;
 
 				if ((foodType & FoodTypeFlags.Plant) != 0)
 				{
-					if (def == ThingDefOf.Hay)
+					if (def.plant == null)
+					{
+						CategoryScores[FoodCategory.Hay] += 100f;
+					}
+					else
+					{
+						if (def.plant.sowTags != null && def.plant.sowTags.Any())
+							CategoryScores[FoodCategory.Plant] += 100f;
+						else
+							CategoryScores[FoodCategory.Grass] += 50f;
 
-						return FoodCategory.Hay;
+						if (def.plant.harvestedThingDef != null)
+						{
+							CategoryScores[FoodCategory.Plant] += 200f;
+						}
 
-					//TODO: Make more reliable
-					if (defName == "PlantGrass" || defName == "PlantTallGrass")
+						if (def.plant.reproduces)
+							CategoryScores[FoodCategory.Grass] += 100f;
+					}
+				}
 
-						return FoodCategory.Grass;
-
-					return FoodCategory.Plant;
+				if (RimWorld.FoodUtility.IsHumanlikeMeat(def))
+				{
+					CategoryScores[FoodCategory.RawHuman] += 50f;
+					CategoryScores[FoodCategory.HumanlikeCorpse] += 50f;
 				}
 
 				if (def.IsCorpse)
 				{
-					if (RimWorld.FoodUtility.IsHumanlikeMeat(def))
+					CategoryScores[FoodCategory.Corpse] += 80f;
+					CategoryScores[FoodCategory.HumanlikeCorpse] += 50f;
+					CategoryScores[FoodCategory.InsectCorpse] += 50f;
 
-						return FoodCategory.HumanlikeCorpse;
+					if (def.FirstThingCategory == ThingCategoryDefOf.CorpsesInsect)
 
-					//TODO: Make more reliable
-					if (def.ingestible.sourceDef.race.Animal)
-					{
-						if (def.FirstThingCategory == ThingCategoryDefOf.CorpsesInsect)
-
-							return FoodCategory.InsectCorpse;
-
-						return FoodCategory.Corpse;
-					}
+						CategoryScores[FoodCategory.InsectCorpse] += 50f;
 
 					if (def.ingestible.sourceDef.race.IsMechanoid)
 
-						return FoodCategory.Ignore;
+						CategoryScores[FoodCategory.Ignore] = float.MaxValue;
 				}
 
 				if (def.ingestible.tasteThought != null && def.ingestible.tasteThought.stages.All((ThoughtStage arg) => arg.baseMoodEffect < 0))
 				{
-					if (RimWorld.FoodUtility.IsHumanlikeMeat(def))
-
-						return FoodCategory.RawHuman;
-
-					if (def == ThingDef.Named("Megaspider_Meat"))
-						//if (def.ingestible.tasteThought == ThoughtDefOf.AteInsectMeatAsIngredient)
-
-						return FoodCategory.RawInsect;
-
-
-					return FoodCategory.RawBad;
+					//if (def == ThingDef.Named("Megaspider_Meat"))
+					if (def.ingestible.tasteThought == ThoughtDefOf.AteInsectMeatDirect)
+					{
+						CategoryScores[FoodCategory.InsectCorpse] += 50f;
+						CategoryScores[FoodCategory.RawInsect] += 50f;
+					}
+					CategoryScores[FoodCategory.RawHuman] += 20f;
+					CategoryScores[FoodCategory.RawBad] += 50f;
 				}
 
 				if ((def.ingestible.tasteThought == null || def.ingestible.tasteThought.stages.All((ThoughtStage arg) => arg.baseMoodEffect >= 0)))
+					
+					CategoryScores[FoodCategory.RawTasty] += 20f;
 
-					return FoodCategory.RawTasty;
-
-				if ((foodType & FoodTypeFlags.AnimalProduct) != 0)
-
-					return FoodCategory.AnimalProduct;
+				//if ((foodType & FoodTypeFlags.AnimalProduct) != 0)
+					
+				//	CategoryScores[FoodCategory.AnimalProduct] += 30f;
 
 				if (foodPref == FoodPreferability.NeverForNutrition || def.IsDrug)
 
-					return FoodCategory.Ignore;
+					CategoryScores[FoodCategory.Ignore] = float.MaxValue;
 			}
 
 			// non ingestible corpse ?
-			if (def.IsCorpse)
-				return FoodCategory.Ignore;
+			//if (def.IsCorpse)
+			//	return FoodCategory.Ignore;
 
-			return FoodCategory.Null;
+			var winner = CategoryScores.MaxBy(arg => arg.Value);
+
+			var similar = CategoryScores.Where(arg => Mathf.Abs(arg.Value - winner.Value) < 10f);
+
+			if (similar.Count() > 1)
+			{
+				Log.Warning("I'm not sure if " + def + " belongs to " + winner + " since others have similar scores: " + String.Join(" ; ", similar.Select(arg => arg.ToString()).ToArray()));
+			}
+			return winner.Key;
 		}
 	}
 }
