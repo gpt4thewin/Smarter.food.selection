24a5158005129285e318cf852e0fd31409e098b2
juliencsharp <juliencsharp@github.com>
2018-10-20 12:38:53 +0200

    Fix mod breaking missing harmony reference. You will have to updates you references again :)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,7 +18,6 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 //[assembly: AssemblyTitle("WM.Smarter.Food.Selection")]
-[assembly: AssemblyVersion("2.3.0.3")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
